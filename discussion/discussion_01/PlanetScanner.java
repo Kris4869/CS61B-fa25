d0edfd94a9f234636c96a0622353c4900d4ed9f1
Kris4869 <kidkris4869@gmail.com>
2025-11-19 10:30:47 +0800

    comments on discussion_01

--- discussion/discussion_01/PlanetScanner.java
+++ discussion/discussion_01/PlanetScanner.java
@@ -3,21 +3,27 @@ public class PlanetScanner {
     Planet[] scanned_Planets;
 
     public PlanetScanner(Planet[] planets, int numToScan){
+        // initialize variables
         this.planets = planets;
         this.scanned_Planets = new Planet[0];
+
         this.scanMorePlanets(numToScan);
     }
 
     public Planet[] scannedPlants(){
+        // reture a variable
         return this.scanned_Planets;
     }
 
     public void scanMorePlanets(int numToScan){
+        // create a new scanned_Planets array
         int newSize = this.scanned_Planets.length + numToScan;
         Planet[] newScan = new Planet[newSize];
+
         for (int i = 0; (i < newSize) && (i < this.planets.length); i++){
             newScan[i] = this.planets[i];
         }
+        // renew variable scanned_Planets
         this.scanned_Planets = newScan;
     }
 
